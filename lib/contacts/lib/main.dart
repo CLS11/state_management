d2431497d6afa04cf762e197e43f82baaa5fa966
CLS11 <crystall.101600@gmail.com>
2024-12-25 14:55:17 +0530

    Contains a route to viewing the contacts along with navigating to Home page.

---
+++ lib/contacts/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:contacts/home_page.dart';
+import 'package:contacts/new_contact.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+      routes: {
+        '/new-contact': (context) => const NewContactView(),
+      },
+    ),
+  );
+}
