150e7dab27de2046f1edbfce886b134fe1aa4c71
CLS11 <crystall.101600@gmail.com>
2024-12-26 17:41:55 +0530

    Api which grabs the current date and time and returns it.

---
+++ lib/stateful_app/lib/api_provider.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:stateful_app/api.dart';
+import 'package:uuid/uuid.dart';
+
+class ApiProvider extends InheritedWidget {
+  ApiProvider({
+    required this.api,
+    required super.child,
+    super.key,
+  }) : uuid = const Uuid().v4();
+  final Api api;
+  final String uuid;
+
+  @override
+  bool updateShouldNotify(covariant ApiProvider oldWidget) {
+    return uuid != oldWidget.uuid;
+  }
+
+  static ApiProvider of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<ApiProvider>()!;
+  }
+}
