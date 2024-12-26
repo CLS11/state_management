219257a9eeb9254db5e6315e480cfdd7da3ef164
CLS11 <crystall.101600@gmail.com>
2024-12-26 17:43:21 +0530

    Api which grabs the current date and time and returns it.

---
+++ lib/stateful_app/lib/api.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Api {
+  String? dateAndTime;
+
+  Future<String> getDateAndTime() {
+    return Future.delayed(
+      const Duration(seconds: 1),
+      () => DateTime.now().toIso8601String(),
+    ).then((value) {
+      dateAndTime = value;
+      return value;
+    });
+  }
+}
