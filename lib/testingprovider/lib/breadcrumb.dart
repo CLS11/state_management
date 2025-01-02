8be04c83c05196ece19d17d30ae99693fadc7dbb
CLS11 <crystall.101600@gmail.com>
2025-01-02 14:33:53 +0530

    Contains the properties of breadcrumb.

---
+++ lib/testingprovider/lib/breadcrumb.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
+
+class BreadCrumb {
+  BreadCrumb({
+    required this.isActive,
+    required this.name,
+    required this.uuid,
+  });
+
+  bool isActive;
+  final String name;
+  final String uuid;
+
+  //Function which turns the status of prior bread crumb as true
+  void activate() {
+    isActive = true;
+  }
+
+  @override
+  bool operator ==(covariant BreadCrumb other) => uuid == other.uuid;
+
+  @override
+  int get hashCode => uuid.hashCode;
+
+  //isActive == other.isActive && name == other.name;
+
+  String get title => name + (isActive ? ' > ' : ' ');
+}
