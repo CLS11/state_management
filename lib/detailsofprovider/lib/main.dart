69e78404245299d3dea9774c88a573f979b88b16
CLS11 <crystall.101600@gmail.com>
2025-01-04 19:24:51 +0530

    Added a change notifier provider.

--- lib/detailsofprovider/lib/main.dart
+++ lib/detailsofprovider/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:detailsofprovider/home_page.dart';
+import 'package:detailsofprovider/object_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,9 +12,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
+    return ChangeNotifierProvider(
+      create: (_) => ObjectProvider(),
+      child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: HomePage(),
+      ),
     );
   }
 }
