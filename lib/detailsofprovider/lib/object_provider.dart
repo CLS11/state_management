679ea5bb09d536764cf5909ea8164ffb864c2c4b
CLS11 <crystall.101600@gmail.com>
2025-01-04 19:29:04 +0530

    Contains the change notifier with methods start and stop.

--- lib/detailsofprovider/lib/object_provider.dart
+++ lib/detailsofprovider/lib/object_provider.dart
@@ -2,8 +2,22 @@ import 'dart:async';
 
 import 'package:detailsofprovider/base_object.dart';
 import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
 
 class ObjectProvider extends ChangeNotifier {
+  ObjectProvider()
+      : id = const Uuid().v4(),
+        _cheapObject = CheapObject(),
+        _expensiveObject = ExpensiveObject(){
+          start();
+        }
+
+  @override
+  void notifyListeners() {
+    id = const Uuid().v4();
+    super.notifyListeners();
+  }
+
   late String id;
   late CheapObject _cheapObject;
   late StreamSubscription _cheapObjectStreamSubs;
@@ -12,4 +26,24 @@ class ObjectProvider extends ChangeNotifier {
 
   CheapObject get cheapObject => _cheapObject;
   ExpensiveObject get expensiveObject => _expensiveObject;
+
+  void start() {
+    _cheapObjectStreamSubs = Stream.periodic(
+      const Duration(seconds: 1),
+    ).listen((_) {
+      _cheapObject = CheapObject();
+      notifyListeners();
+    });
+    _expensiveObjectStreamSubs = Stream.periodic(
+      const Duration(seconds: 10),
+    ).listen((_) {
+      _expensiveObject = ExpensiveObject();
+      notifyListeners();
+    });
+  }
+
+  void stop() {
+    _cheapObjectStreamSubs.cancel();
+    _expensiveObjectStreamSubs.cancel();
+  }
 }
