4cc4f65b95089afdec7bfdf1a9d147135f7d2294
CLS11 <crystall.101600@gmail.com>
2025-01-04 19:25:34 +0530

    Home page with the representation of all 3 widgets.

--- lib/detailsofprovider/lib/home_page.dart
+++ lib/detailsofprovider/lib/home_page.dart
@@ -1,4 +1,9 @@
+import 'package:detailsofprovider/cheap_widget.dart';
+import 'package:detailsofprovider/expensive_widget.dart';
+import 'package:detailsofprovider/object_provider.dart';
+import 'package:detailsofprovider/object_provider_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,8 +11,52 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: const Text(
+          'Home Page',
+        ),
+      ),
+      body: Column(
+        children: [
+          const Row(
+            children: [
+              Expanded(
+                child: CheapWidget(),
+              ),
+              Expanded(
+                child: ExpensiveWidget(),
+              ),
+            ],
+          ),
+          const Row(
+            children: [
+              Expanded(
+                child: ObjectProviderWidget(),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              TextButton(
+                onPressed: () {
+                  context.read<ObjectProvider>().stop();
+                },
+                child: const Text(
+                  'Stop',
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  context.read<ObjectProvider>().start();
+                },
+                child: const Text(
+                  'Start',
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
-
