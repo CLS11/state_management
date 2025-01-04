54448c52836816757d7ec7dd247dd86a3369377a
CLS11 <crystall.101600@gmail.com>
2025-01-04 19:28:35 +0530

    Widget with all the functioning of object provider.

---
+++ lib/detailsofprovider/lib/object_provider_widget.dart
@@ -0,0 +1,27 @@
+import 'package:detailsofprovider/object_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class ObjectProviderWidget extends StatelessWidget {
+  const ObjectProviderWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final provider = context.watch<ObjectProvider>();
+    return Container(
+      height: 100,
+      color: Colors.purple,
+      child: Column(
+        children: [
+          const Text(
+            'Object Provider Widget',
+          ),
+          const Text(
+            'ID',
+          ),
+          Text(provider.id),
+        ],
+      ),
+    );
+  }
+}
