b3b36099ec7fa3ce669485ee59a0d726aab0eeb8
CLS11 <crystall.101600@gmail.com>
2025-01-05 21:03:08 +0530

    Compact map.

---
+++ lib/testingflutterhooks/lib/compactmap.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+extension CompactMap<T> on Iterable<T?> {
+  Iterable<T> compactMap<E>([
+    E? Function(T?)? transform,
+  ]) =>
+      map(
+        transform ?? (e) => e,
+      ).where((e) => e != null).cast();
+}
+
+/*void testIt() {
+  final values = [1, 2, null, 3];
+  final nonNullValues = values.compactMap((e) {
+    if (e != null && e > 10) {
+      return e;
+    } else {
+      return null;
+    }
+  });
+}*/
