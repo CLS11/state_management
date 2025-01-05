83540211b06ab9c7019d9dc9f51724ae90c87bc1
CLS11 <crystall.101600@gmail.com>
2025-01-05 21:47:19 +0530

    Utilised useMemoized and useListenable.

--- lib/testingflutterhooks/lib/home_page.dart
+++ lib/testingflutterhooks/lib/home_page.dart
@@ -1,29 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:testingflutterhooks/compactmap.dart';
-import 'package:testingflutterhooks/url.dart';
+import 'package:testingflutterhooks/countdown.dart';
 
 class HomePage extends HookWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final future = useMemoized(
-      () => NetworkAssetBundle(Uri.parse(url))
-          .load(url)
-          .then((data) => data.buffer.asUint8List())
-          .then(Image.memory),
-    );
-    final snapshot = useFuture(future);
+    final countDown = useMemoized(() => CountDown(from: 20));
+    final notifier = useListenable(countDown);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Page'),
       ),
-      body: Column(
-        children: [
-          snapshot.data,
-        ].compactMap().toList(),
+      body: Text(
+        notifier.value.toString(),
       ),
     );
   }
