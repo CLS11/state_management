e1b812cdf6ecd5005313e0d85fb7d0b9fccc0ae4
CLS11 <crystall.101600@gmail.com>
2025-01-05 21:04:11 +0530

    Utilised useMemoized and useFuture.

--- lib/testingflutterhooks/lib/home_page.dart
+++ lib/testingflutterhooks/lib/home_page.dart
@@ -1,30 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:testingflutterhooks/compactmap.dart';
+import 'package:testingflutterhooks/url.dart';
 
 class HomePage extends HookWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final controller = useTextEditingController();
-    final text = useState('');
-    useEffect(() {
-      controller.addListener(() {
-        text.value = controller.text;
-      });
-      return null;
-    });
+    final future = useMemoized(
+      () => NetworkAssetBundle(Uri.parse(url))
+          .load(url)
+          .then((data) => data.buffer.asUint8List())
+          .then(Image.memory),
+    );
+    final snapshot = useFuture(future);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Page'),
       ),
       body: Column(
         children: [
-          TextField(
-            controller: controller,
-          ),
-          Text('You typed ${text.value}'),
-        ],
+          snapshot.data,
+        ].compactMap().toList(),
       ),
     );
   }
