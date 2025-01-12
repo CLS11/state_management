22e48605f487497ea9d958fd3ad24cd21dd3f078
CLS11 <crystall.101600@gmail.com>
2025-01-12 23:45:09 +0530

    Added a hook widget.

--- lib/reduxexample/lib/home_page.dart
+++ lib/reduxexample/lib/home_page.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:redux/redux.dart' as redux;
+import 'package:reduxexample/app_state.dart';
+import 'package:reduxexample/appstate_reducer.dart';
+import 'package:reduxexample/item_filter.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends HookWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final redux.Store<AppState> store = redux.Store<AppState>(
+      appStateReducer,
+      initialState: const AppState(
+        items: [],
+        filter: ItemFilter.all,
+      ),
+    );
     return Scaffold(
       appBar: AppBar(),
     );
